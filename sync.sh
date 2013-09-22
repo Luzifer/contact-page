719d3c1d35121f7a3dd290e550571a5af19b83c3
Knut Ahlers <knut@ahlers.me>
2013-09-22 14:42:54 +0200

    Added caching for images

--- sync.sh
+++ sync.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-s3cmd sync --exclude=".git/*" --delete-removed -P ./ s3://www.knut.mp/
+s3cmd sync --exclude=".git/*" --exclude="images/*" -P ./ s3://www.knut.mp/
+
+imgexpire=$(python -c "from datetime import *; print (datetime.utcnow() + timedelta(days=1)).strftime('%a, %d %b %Y %H:%M:%S GMT')")
+s3cmd sync -P --add-header="Expires:$imgexpire" ./images/ s3://www.knut.mp/images/
