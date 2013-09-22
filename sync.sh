7c55a34f0f3d66b74167cc4ad591598d0da2381e
Knut Ahlers <knut@ahlers.me>
2013-09-22 15:24:25 +0200

    Realized hints from gtmetrix

--- sync.sh
+++ sync.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-s3cmd sync --exclude=".git/*" --exclude="images/*" -P ./ s3://www.knut.mp/
+minify style.css style.min.css
+s3cmd sync --exclude=".git/*" --exclude="images/*" --exclude="lib/*" -P ./ s3://www.knut.mp/
+rm style.min.css
 
-imgexpire=$(python -c "from datetime import *; print (datetime.utcnow() + timedelta(days=1)).strftime('%a, %d %b %Y %H:%M:%S GMT')")
+imgexpire=$(python -c "from datetime import *; print (datetime.utcnow() + timedelta(days=8)).strftime('%a, %d %b %Y %H:%M:%S GMT')")
+s3cmd sync -P --add-header="Expires:$imgexpire" ./lib/ s3://www.knut.mp/lib/
+
+imgexpire=$(python -c "from datetime import *; print (datetime.utcnow() + timedelta(days=8)).strftime('%a, %d %b %Y %H:%M:%S GMT')")
 s3cmd sync -P --add-header="Expires:$imgexpire" ./images/ s3://www.knut.mp/images/
