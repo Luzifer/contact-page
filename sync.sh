c03b04cb9e23b65afb68599bf7d6d3184be7b757
Knut Ahlers <knut@ahlers.me>
2013-09-22 22:12:00 +0200

    Switched to generated content using swig

--- sync.sh
+++ sync.sh
@@ -2,9 +2,14 @@
 
 find . -name '.DS_Store' -delete
 
-minify style.css style.min.css
-s3cmd sync --exclude=".git/*" --exclude="images/*" --exclude="lib/*" --exclude="sync.sh" --exclude="style.css" -P ./ s3://www.knut.mp/
-rm style.min.css
+mkdir rendered
+for template in $(find templates -name 'tpl_*'); do
+  swig render $template > rendered/$(basename $template | sed 's/tpl_//')
+done
+minify style.css rendered/style.min.css
+
+s3cmd sync --exclude=".git/*" --exclude="images/*" --exclude="lib/*" --exclude="sync.sh" --exclude="style.css" -P ./rendered/ s3://www.knut.mp/
+rm -rf rendered
 
 imgexpire=$(python -c "from datetime import *; print (datetime.utcnow() + timedelta(days=8)).strftime('%a, %d %b %Y %H:%M:%S GMT')")
 s3cmd sync -P --add-header="Expires:$imgexpire" ./lib/ s3://www.knut.mp/lib/
