67e808652822ec7dcfa4acb08a31727b4f40695d
Knut Ahlers <knut@ahlers.me>
2013-10-19 11:50:30 +0200

    Added file deletion for sync of lib and images

--- sync.sh
+++ sync.sh
@@ -14,7 +14,7 @@ s3cmd sync --exclude=".git/*" --exclude="images/*" --exclude="lib/*" --exclude="
 rm -rf rendered
 
 imgexpire=$(python -c "from datetime import *; print (datetime.utcnow() + timedelta(days=8)).strftime('%a, %d %b %Y %H:%M:%S GMT')")
-s3cmd sync -P --add-header="Expires:$imgexpire" ./lib/ s3://www.knut.mp/lib/
+s3cmd sync -P --add-header="Expires:$imgexpire" --delete-removed ./lib/ s3://www.knut.mp/lib/
 
 imgexpire=$(python -c "from datetime import *; print (datetime.utcnow() + timedelta(days=8)).strftime('%a, %d %b %Y %H:%M:%S GMT')")
-s3cmd sync -P --add-header="Expires:$imgexpire" ./images/ s3://www.knut.mp/images/
+s3cmd sync -P --add-header="Expires:$imgexpire" --delete-removed ./images/ s3://www.knut.mp/images/
