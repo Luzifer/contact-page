38e12d9d38f18fa82997846da032b2f67d55ed6a
Knut Ahlers <knut@ahlers.me>
2013-12-24 15:50:34 +0100

    Added sync for additional files

--- sync.sh
+++ sync.sh
@@ -16,6 +16,8 @@ rm -rf node_modules
 s3cmd sync --exclude=".git/*" --exclude="images/*" --exclude="lib/*" --exclude="sync.sh" --exclude="style.css" -P ./rendered/ s3://knut-ahlers.de/
 rm -rf rendered
 
+s3cmd sync -P ./files/ s3://knut-ahlers.de/files/
+
 imgexpire=$(python -c "from datetime import *; print (datetime.utcnow() + timedelta(days=8)).strftime('%a, %d %b %Y %H:%M:%S GMT')")
 s3cmd sync -P --add-header="Expires:$imgexpire" --delete-removed ./lib/ s3://knut-ahlers.de/lib/
 
